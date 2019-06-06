5534029e8b08c14cc9dcb5f64cef6c515ca9cf65
lblazok <lblazok3@gmail.com>
2019-06-06 13:21:59 +0200

    original

---
+++ calc_volume.py
@@ -0,0 +1,40 @@
+def volumen_zasipa(d1, d2, h1, h2):
+    '''
+    Izracun potrebnog volumena zasipa
+    ulazni podaci: fi bušenja i fi ugradnje, dubina bušenja i dubina ugradnje
+    d1 = fi bušenja [m]
+    d2 = fi ugradnje [m]
+    h1 = dubina bušenja [m]
+    h2 = dubina ugradnje [m]
+    '''
+    import math
+    v1 = math.pi * (d1/2)**2 * h1
+    v2 = math.pi * (d2/2)**2 *h2
+
+    ans_v = float("{0:.2f}".format(v1 - v2))
+
+    if ans_v < 0:
+        return "Error - provjeri unesene podatke"
+    else:
+        return ans_v
+    
+i = int(input('Unesi broj profila bušenja/ugradnje = '))
+
+def unos_pod(i):
+    SUB = str.maketrans("0123456789", "₀₁₂₃₄₅₆₇₈₉")
+
+    d1 = float(input('Unesi fi ' + str(i+1).translate(SUB) +' busenja [m] =  '))
+    d2 = float(input('Unesi fi '+ str(i+1).translate(SUB) +' ugradnje [m] = '))
+    h1 = float(input('Unesi dubinu '+ str(i+1).translate(SUB) +' busenja [m] = '))
+    h2 = float(input('Unesi dubinu '+ str(i+1).translate(SUB) +' ugradnje [m] = ' ))
+
+    ans = volumen_zasipa(d1, d2, h1, h2)
+    
+    return ans
+ans = 0
+for x in range(i):
+    ans += unos_pod(x)
+    
+print(ans)
+
+input('Pritisni "Enter" za izlaz')
