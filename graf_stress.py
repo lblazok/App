3855b17041c0a8e5f1a200e61ca5fe7f666f9225
Lovro Blazok <lblazok3@gmail.com>
2019-06-18 19:52:37 +0200

    pocetak grafa

---
+++ graf_stress.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt 
+
+def plot(x, y):
+    '''
+    Crtanje grafova naprezanja
+    Ulaz su array s podacima o dubini i vrijednostima naprezanja
+    '''
+
+    plt.plot(x, y)
